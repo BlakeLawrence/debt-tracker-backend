b88f9c679058c6d813a79823a2d08a6efd19b894
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-08-12 09:36:10 +0100

    added cors and got front end communicating with backend

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
+import cors from "cors";
 
 import financialsRouter from "./routes/financials.js";
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/financials", financialsRouter);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
